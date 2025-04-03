ecfd57c7676672902bfb0fcd52f1008cf6182bab
Nguyen Thanh Nam <94415188+mareZ-noob@users.noreply.github.com>
2025-04-04 00:53:43 +0700

    Update set_up_hosts.sh

--- set_up_hosts.sh
+++ set_up_hosts.sh
@@ -1,6 +1,5 @@
 echo 127.0.0.1 postgres | sudo tee -a /etc/hosts
 echo 127.0.0.1 pgadmin | sudo tee -a /etc/hosts
-echo 127.0.0.1 postgres | sudo tee -a /etc/hosts
 echo 127.0.0.1 zk | sudo tee -a /etc/hosts
 echo 127.0.0.1 kafka | sudo tee -a /etc/hosts
 echo 127.0.0.1 es | sudo tee -a /etc/hosts    
